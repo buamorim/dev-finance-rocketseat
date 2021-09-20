3472e31eceba92a2e69d12cbfca21c775ede7c26
buamorim <brunamorim.silva@outlook.com>
2021-09-20 18:32:42 -0300

    js add

---
+++ app.js
@@ -0,0 +1,14 @@
+let Modal = {
+    open (){
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .add('active')
+    },
+    close (){
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+    }
+}
