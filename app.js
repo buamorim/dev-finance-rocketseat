18399b2401f92e0577d7049fe8780d29367a6e9f
buamorim <brunamorim.silva@outlook.com>
2021-09-23 11:21:14 -0300

    mode add and remove js

--- app.js
+++ app.js
@@ -12,3 +12,53 @@ let Modal = {
         .remove('active')
     }
 }
+
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
+
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if( transaction.amount > 0 ) {
+                income += transaction.amount;
+            }
+        })
+        return income;
+    },
+
+    expenses() {
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if( transaction.amount < 0 ) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
+    },
+
+    total() {
+        return Transaction.incomes() + Transaction.expenses();
+    }
+}
